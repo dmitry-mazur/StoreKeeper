f196b6db0b800814677956bcac85fac6ed1ac81c
Dmitry <d.mazur@desoft.technology>
2019-03-04 18:07:26 +0600

    Initial Commit

---
+++ StoreKeeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreKeeper
+//
+//  Created by Dmitry Mazur on 04/03/2019.
+//  Copyright © 2019 Dmitry Mazur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
